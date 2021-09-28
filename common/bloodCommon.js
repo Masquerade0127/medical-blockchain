a0f6dad60616351312efe46f525e2953bab92306
masquerade0127 <masquerade236@gmail.com>
2021-09-28 16:00:54 +0700

    update bloody-contract

---
+++ common/bloodCommon.js
@@ -0,0 +1,15 @@
+'use strict';
+
+class BloodCommon {
+    bloodValidation(bloodObject){
+        if(!bloodObject){
+            throw new Error('blood object null');
+        }
+
+        if(!arguments[0]){
+            
+        }
+    }
+}
+
+module.exports = BloodCommon;
