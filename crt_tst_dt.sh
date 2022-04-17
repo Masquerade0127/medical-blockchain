1a6bed0ed61374f4f4b56066f0479f7ca894acb6
masquerade le <masquerade236@gmail.com>
2022-04-17 17:39:12 +0700

    update contract

--- crt_tst_dt.sh
+++ crt_tst_dt.sh
@@ -9,13 +9,13 @@ export CORE_PEER_ADDRESS=localhost:7051
 
 export name="medical"
 
-# for i in $(seq 1 500000)
-# do
-# echo "create package data " $i
-# peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n $name --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" -c '{"function":"createPackageData","Args":["'$i'","package '$i'","20220213","spl'$i'","supplier '$i'"]}'
-# done
+for i in $(seq 1 100000)
+do
+echo "create package data " $i
+peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n $name --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" -c '{"function":"createPackageData","Args":["'$i'","package '$i'","20220213","spl'$i'","supplier '$i'"]}'
+done
 
-for i in $(seq 1 90000)
+for i in $(seq 1 100000)
 do
 echo "create equipment data " $i
 peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C mychannel -n $name --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" -c '{"function":"createEquipment","Args":["eqm'$i'","equipment '$i'","100","cai","pkg'$i'"]}'
